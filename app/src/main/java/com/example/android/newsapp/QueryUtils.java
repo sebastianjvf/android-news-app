dfe23246235c2ec6f0e3ef2f62080c577ef391ea
sebastianjvf <10047596+sebastianjvf@users.noreply.github.com>
2017-11-15 21:44:18 +0100

    feat: Implemented reading from the server. Watch out, still running on the main thread so will throw an error currently.

--- app/src/main/java/com/example/android/newsapp/QueryUtils.java
+++ app/src/main/java/com/example/android/newsapp/QueryUtils.java
@@ -1,10 +1,22 @@
 package com.example.android.newsapp;
 
+import android.util.Log;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
+
+import static com.example.android.newsapp.MainActivity.LOG_TAG;
 
 public class QueryUtils {
 
@@ -14,12 +26,12 @@ public class QueryUtils {
     public QueryUtils() {
     }
 
-    protected static ArrayList<NewsArticle> extractNewsArticles(String JSON) {
+    protected static ArrayList<NewsArticle> extractNewsArticles(String inputUrl) {
         ArrayList<NewsArticle> newsArticles = new ArrayList<NewsArticle>();
 
         try {
             // Parse the given JSON and retrieve the array of news articles.
-            JSONObject parsedJSON = (new JSONObject(JSON)).optJSONObject("response");
+            JSONObject parsedJSON = (new JSONObject(makeHttpRequest(inputUrl))).optJSONObject("response");
 
             JSONArray newsArticleJSONArray = (JSONArray) parsedJSON.optJSONArray("results");
 
@@ -39,6 +51,93 @@ public class QueryUtils {
         }
 
         return newsArticles;
+    }
+
+    /**
+     * Returns a JSON formatted String from the given url.
+     *
+     * @param urlString
+     * @return
+     */
+    protected static String makeHttpRequest(String urlString) {
+        URL url = null;
+
+        // Convert to URL
+        try {
+            url = new URL(urlString);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        String jsonResponse = null;
+
+        // If there was an error, return null.
+        if (url == null) {
+            return jsonResponse;
+        }
+
+        HttpURLConnection urlConnection = null;
+        InputStream inputStream = null;
+
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setReadTimeout(10000 /* milliseconds */);
+            urlConnection.setConnectTimeout(15000 /* milliseconds */);
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            // If the request was successful (response code 200),
+            // then read the input stream and parse the response.
+            if (urlConnection.getResponseCode() == 200) {
+                inputStream = urlConnection.getInputStream();
+                jsonResponse = readFromStream(inputStream);
+            } else {
+                Log.e(LOG_TAG, "Error response code: " + urlConnection.getResponseCode());
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return jsonResponse;
+    }
+
+    /**
+     * Converts the InputStream into a String which contains the
+     * whole JSON response from the server.
+     *
+     * @param inputStream
+     * @return
+     * @throws IOException
+     */
+    private static String readFromStream(InputStream inputStream) throws IOException {
+        StringBuilder output = new StringBuilder();
+        if (inputStream != null) {
+            // Takes pure data as an input stream
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+
+            // Provides meaning by converting the input stream to characters
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+
+            // After one line was read, readLine() points to the next
+            String line = reader.readLine();
+            while (line != null) {
+                output.append(line);
+                line = reader.readLine();
+            }
+        }
+        return output.toString();
     }
 
 }
