eb5b8de755e74617deaef8fef9f1f48f346c0b0f
sebastianjvf <10047596+sebastianjvf@users.noreply.github.com>
2017-11-15 13:42:19 +0100

    Initial commit

---
+++ app/src/main/java/com/example/android/newsapp/QueryUtils.java
@@ -0,0 +1,11 @@
+package com.example.android.newsapp;
+
+public class QueryUtils {
+
+    /**
+     * Empty constructor to prohibit any instances from being created.
+     */
+    public QueryUtils() {
+    }
+
+}
