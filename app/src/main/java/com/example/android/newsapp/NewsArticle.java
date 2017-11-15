31c992ba1f85e6d9cebb616b5f4afe60fdadbe8c
sebastianjvf <10047596+sebastianjvf@users.noreply.github.com>
2017-11-15 14:34:21 +0100

    feat: Read data from JSON.
    Also implemented toString for NewsArticle.

--- app/src/main/java/com/example/android/newsapp/NewsArticle.java
+++ app/src/main/java/com/example/android/newsapp/NewsArticle.java
@@ -4,10 +4,12 @@ public class NewsArticle {
 
     String title;
     String date;
+    String url;
 
-    public NewsArticle(String title, String date) {
+    public NewsArticle(String title, String date, String url) {
         this.title = title;
         this.date = date;
+        this.url = url;
     }
 
     public void setTitle(String title) {
@@ -18,11 +20,24 @@ public class NewsArticle {
         this.date = date;
     }
 
+    public void setUrl(String url) {
+        this.date = date;
+    }
+
     public String getTitle() {
         return title;
     }
 
     public String getDate() {
         return date;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    @Override
+    public String toString() {
+        return "Title: " + title + ", Date: " + date + ", URL: " + url;
     }
 }
--- app/src/main/java/com/example/android/newsapp/QueryUtils.java
+++ app/src/main/java/com/example/android/newsapp/QueryUtils.java
@@ -1,6 +1,7 @@
 package com.example.android.newsapp;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
@@ -16,9 +17,26 @@ public class QueryUtils {
     protected static ArrayList<NewsArticle> extractNewsArticles(String JSON) {
         ArrayList<NewsArticle> newsArticles = new ArrayList<NewsArticle>();
 
-        // Parse the given JSON and retrieve the array of news articles.
-        JSONObject parsedJSON = new JSONObject();
-        JSONArray newsArticleJSONArray = parsedJSON.optJSONArray("results");
+        try {
+            // Parse the given JSON and retrieve the array of news articles.
+            JSONObject parsedJSON = (new JSONObject(JSON)).optJSONObject("response");
+
+            JSONArray newsArticleJSONArray = (JSONArray) parsedJSON.optJSONArray("results");
+
+            for (int i = 0; i < newsArticleJSONArray.length(); i++) {
+                // Create a new NewsArticle and add data to the list
+                JSONObject currentItem = (JSONObject) newsArticleJSONArray.get(i);
+                String title = currentItem.getString("webTitle");
+                String date = currentItem.getString("webPublicationDate");
+                String url = currentItem.getString("webUrl");
+
+                NewsArticle currentNewsArticle = new NewsArticle(title, date, url);
+                newsArticles.add(currentNewsArticle);
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
         return newsArticles;
     }
